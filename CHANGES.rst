56b9694f539376a000be71e0e719dbe363d77973
Jens Vagelpohl <jens@plyp.com>
2025-11-16 15:31:25 +0100

    - move metadata into ``pyproject.toml``, run zope.meta

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,8 +1,10 @@
-Changelog
-=========
+Change log
+==========
 
 5.4 (unreleased)
 ----------------
+
+- Move all supported package metadata into ``pyproject.toml``.
 
 
 5.3 (2025-11-15)
@@ -125,20 +127,24 @@ Changes since 2.13.2:
 
 - Add AppVeyor configuration to automate building Windows eggs.
 
+
 2.13.2 (2010-06-16)
 -------------------
 
 - LP #587760: Handle tp_basicsize correctly.
+
 
 2.13.1 (2010-04-30)
 -------------------
 
 - Removed undeclared testing dependency on zope.testing.
 
+
 2.13.0 (2010-02-23)
 -------------------
 
 - Update to include ExtensionClass 2.13.0.
+
 
 2.12.0 (2010-02-14)
 -------------------
@@ -149,6 +155,7 @@ Changes since 2.13.2:
 - Updated C includes to ExtensionClass 2.12.0.
 
 - Updated package metadata and remove old build artifacts.
+
 
 2.11.1 (2009-02-19)
 -------------------
--- setup.py
+++ setup.py
@@ -2,12 +2,8 @@ import os
 import platform
 
 from setuptools import Extension
-from setuptools import find_packages
 from setuptools import setup
 
-
-README = open('README.rst').read()
-CHANGES = open('CHANGES.rst').read()
 
 # PyPy won't build the extension
 py_impl = getattr(platform, 'python_implementation', lambda: None)
@@ -21,47 +17,5 @@ else:
                   include_dirs=['include', 'src']),
     ]
 
-version = '5.4.dev0'
 
-setup(
-    name='Persistence',
-    version=version,
-    url='https://github.com/zopefoundation/Persistence',
-    license='ZPL-2.1',
-    description='Persistent ExtensionClass',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    long_description='\n\n'.join([README, CHANGES]),
-    long_description_content_type='text/x-rst',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    classifiers=[
-        "Development Status :: 6 - Mature",
-        "Environment :: Web Environment",
-        "Framework :: Zope",
-        "Framework :: Zope :: 4",
-        "Framework :: Zope :: 5",
-        "License :: OSI Approved :: Zope Public License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-    ext_modules=ext_modules,
-    python_requires='>=3.10',
-    install_requires=[
-        'ExtensionClass >= 4.6',
-        'persistent >= 4.1.1',
-    ],
-    extras_require={
-        'test': ['zope.testrunner'],
-    },
-    include_package_data=True,
-    zip_safe=False,
-)
+setup(ext_modules=ext_modules)
